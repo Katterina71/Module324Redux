b19db79a491a226ebb00f5d3486fd5246a89717d
Kate Iliu <beviatori@gmail.com>
2024-05-23 11:41:09 -0600

    Add router

--- FullstackApp/todos-fullstack/backend/server.js
+++ FullstackApp/todos-fullstack/backend/server.js
@@ -2,12 +2,27 @@ import express from 'express'
 import  'dotenv/config'
 
 import mongoConfig from './config.js'
+import Todo from './models/TodoModel.js'
 
 const app = express();
 const PORT = 8080
 
 app.get('/', (req,res) => {
  res.json('Hello! (from Server)')
+})
+
+
+app.get('/api/todos', async (req,res)=>{
+    try {
+       const todos = await Todo.find()
+       res.status(200)
+          .json(todos)
+
+        console.log('Success')
+    } catch (error) {
+        console.error('No data fetch', error)
+        res.status(400).json(error)
+    }
 })
 
 app.listen(PORT, ()=> {
