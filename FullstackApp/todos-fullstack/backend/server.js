b6fa959720e099748527e3798eca607aa50caced
Kate Iliu <beviatori@gmail.com>
2024-05-23 14:37:22 -0600

    connect backend to frontend

--- FullstackApp/todos-fullstack/backend/server.js
+++ FullstackApp/todos-fullstack/backend/server.js
@@ -1,4 +1,6 @@
 import express from 'express'
+
+import cors from 'cors' // connect backend and frontend
 import  'dotenv/config'
 
 import mongoConfig from './config.js'
@@ -7,16 +9,16 @@ import todoRoutes from './routes/todoRoutes.js'
 const app = express()
 const PORT = 8080
 
+
+
 //middleware
+app.use(cors()) // connect backend and frontend
 app.use(express.json()) //give access to request body
 app.use('/api/todos', todoRoutes) //url matches with this route
 
 app.get('/', (req,res) => {
  res.json('Hello! (from Server)')
 })
-
-
-
 
 app.listen(PORT, ()=> {
     console.log('Listening on port' + PORT)
